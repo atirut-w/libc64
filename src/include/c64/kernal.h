8d8f53d21639e4832875ee9e9855c66b41b85fc4
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-09-21 19:50:34 +0700

    Implement `setlfs`, `setnam` and `open`

--- src/include/c64/kernal.h
+++ src/include/c64/kernal.h
@@ -4,6 +4,10 @@
 extern "C" {
 #endif
 
+void setlfs(char logical_number, char device_number, char secondary_address);
+void setnam(char namelength, char *name);
+void open();
+
 char chrin(); // Read a byte from the default output.
 void chrout(char c); // Write a byte to the default output.
 
--- src/kernal.c
+++ src/kernal.c
@@ -1,5 +1,26 @@
 #include <c64/kernal.h>
 
+void setlfs(char logical_number, char device_number, char secondary_address)
+{
+    asm(
+        "ldy __rc2\n"
+        "jsr $ffba"
+    );
+}
+
+void setnam(char namelength, char *name)
+{
+    asm(
+        "ldy __rc2\n"
+        "jsr $ffbd"
+    );
+}
+
+void open()
+{
+    asm("jsr ffc0");
+}
+
 char chrin()
 {
     char result;
